6a7cad3ec56e6c8dd22cc81407392980c7d644d7
slopez34 <slopez34@cuc.edu.co>
2024-11-19 13:58:14 -0500

    completo

--- src/app/componentes/contenedor/Inicio.tsx
+++ src/app/componentes/contenedor/Inicio.tsx
@@ -1,3 +1,29 @@
+
 export const Inicio = () => {
-  return <>Soy el inicio Como el de la bienvenida...</>;
+  return (
+    <>
+      {" "}
+      <main>
+        <div className="container py-4">
+          <header className="pb-3 mb-4 border-bottom">
+            <span className="fs-4">Bienvenidos</span>
+          </header>
+
+          <div className="p-5 mb-4 bg-body-tertiary rounded-3">
+            <div className="container-fluid py-5">
+              <h1 className="display-5 fw-bold">Bienvenido</h1>
+              <p className="col-md-8 fs-4">
+                Bienvenidos a Libros, tu destino definitivo para todo lo
+                relacionado con el mundo de los libros. Nuestra misión es
+                fomentar el amor por la lectura y proporcionar un espacio donde
+                los amantes de los libros puedan descubrir, compartir y
+                disfrutar de sus obras favoritas. ¡Gracias por visitarnos y
+                esperamos que disfrutes de tu tiempo en Libros!
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 };
--- src/app/componentes/otros/Acerca.tsx
+++ src/app/componentes/otros/Acerca.tsx
@@ -1,10 +1,22 @@
 export const Acerca = () => {
-    return (
-
-        <>
-            Soy el Acerca<br />
-        
-
-        </>
-    )
-}
+  return (
+    <>
+      <div className="bg-body-tertiary p-5 rounded">
+        <div className="col-sm-8 py-5 mx-auto">
+          <h1 className="display-5 fw-normal">Quienes Somos</h1>
+          <p>
+            Nuestro equipo está compuesto por apasionados de la literatura que
+            se dedican a ofrecer contenido de calidad y a crear una comunidad
+            inclusiva y acogedora para todos los lectores.
+            Si deseas mas informacion, dale click al boton "Mas informacion" y nos conoceras mucho mejor.
+          </p>
+          <p>
+            <a className="btn btn-primary" role="button">
+              Mas informacion »
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
